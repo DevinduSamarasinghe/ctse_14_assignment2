0f385cf83ee6bbf807456338c67e91a964b78e96
DevinduSamarasinghe <devindu990@gmail.com>
2024-04-22 21:33:32 +0530

    test: testing ci-cd and merge pass

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express"
 
-var app = express()
-var PORT = process.env.PORT || 80
+const app = express()
+const PORT = process.env.PORT || 80
 
 
 app.get("/", (req,res)=>{
@@ -12,9 +12,6 @@ app.get("/info",(req,res)=>{
     res.send("IT21003332 - Devindu Samarasinghe\nIT21004636 - Nashali Perera\nIT21004322 - Chanukya Serasinghes and sons lol")
 })
 
-app.gangsta("/info", (req,res)=>{
-    res.send("mama gangsta")
-})
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`)
