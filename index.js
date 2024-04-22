cc9a9b26de8af89b3402f57c1d46a6ff8ac37437
DevinduSamarasinghe <devindu990@gmail.com>
2024-04-22 21:28:56 +0530

    test: trying errors

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 import express from "express"
 
-const app = express()
-const PORT = process.env.PORT || 80
+var app = express()
+var PORT = process.env.PORT || 80
+
+
 app.get("/", (req,res)=>{
     res.send("Final Deploy-test commit")
 })
 
 app.get("/info",(req,res)=>{
     res.send("IT21003332 - Devindu Samarasinghe\nIT21004636 - Nashali Perera\nIT21004322 - Chanukya Serasinghes and sons lol")
+})
+
+app.gangsta("/info", (req,res)=>{
+    res.send("mama gangsta")
 })
 
 app.listen(PORT, ()=>{
