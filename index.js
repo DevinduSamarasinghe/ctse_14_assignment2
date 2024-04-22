709d1742ad8669eeee8cc8fb81cc86d2acf4ff3e
DevinduSamarasinghe <devindu990@gmail.com>
2024-04-22 16:41:19 +0530

    feat: rollback deploy-standard - ASG incremental

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from "express"
 const app = express()
 const PORT = process.env.PORT || 80
 app.get("/", (req,res)=>{
-    res.send("Computer Science")
+    res.send("Computer Science 39")
 })
 
 app.get("/info",(req,res)=>{
