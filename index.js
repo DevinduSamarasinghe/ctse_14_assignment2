1f81a1c651bcf000c7f76adc63aebbfa4519b63c
IT21008382 <it21008382@my.sliit.lk>
2024-04-22 22:08:56 +0530

    mongodb implemented

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 import express from "express"
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv/config";
+
+import routers from "./routers/index.js";
 
 const app = express()
 const PORT = process.env.PORT || 80
+app.use(cors());
+app.use(bodyParser.json());
+
+routers(app);
+
+const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL);
 
 
 app.get("/", (req,res)=>{
@@ -9,8 +23,13 @@ app.get("/", (req,res)=>{
 })
 
 app.get("/info",(req,res)=>{
-    res.send("IT21003332 - Devindu Samarasinghe\nIT21004636 - Nashali Perera\nIT21004322 - Chanukya Serasinghes and sons lol")
+    res.send("IT21003332 - Devindu Samarasinghe\nIT21004636 - Nashali Perera\nIT21004322 - Chanukya Serasinghes and sons lol\IT21008382 - Madusha Muthusinghe returns")
 })
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB connection success!");
+});
 
 
 app.listen(PORT, ()=>{
