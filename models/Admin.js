56855d2e88c9ba8b30a319821871b6fdcc517999
IT21008382 <it21008382@my.sliit.lk>
2024-04-23 12:41:35 +0530

    Admin model created

---
+++ models/Admin.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const Schema = mongoose.Schema;
+
+const AdminSchema = new Schema({
+    fname: {
+        type: String,
+        required: true
+    },
+    lname: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    permissionLevel: {
+        type: String,
+        default: "ADMIN",
+    }
+}, {
+    timestamps: {
+        createdAt: 'createdOn',
+        updatedAt: 'updatedOn'
+    }
+});
+
+AdminSchema.pre("save", async function (next) {
+    const user = this;
+    const password = user.password;
+
+    if (!user.isModified("password")) {
+        return next();
+    }
+
+    // Number of rounds hash function will execute
+    const salt = await bcrypt.genSalt(10);
+
+    const hash = bcrypt.hashSync(password, salt);
+    user.password = hash;
+    return next();
+});
+
+const Admin = mongoose.model('Admin', AdminSchema);
+export default Admin;
