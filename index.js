d3195fafe3efb3116cfe719a3ad15a8acb83177e
IT21008382 <it21008382@my.sliit.lk>
2024-04-23 12:22:39 +0530

    made changes

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 import express from "express"
+import bodyParser from "body-parser";
+import cors from "cors";
+
+
+import routers from "./routers/index.js";
 
 const app = express()
 const PORT = process.env.PORT || 80
+app.use(cors());
+app.use(bodyParser.json());
+
+routers(app)
+
+
 app.get("/", (req,res)=>{
-    res.send("First commit API test for CI/CD -2 | additional change of cicd pipeline, pending issues - sonarcloud and blue deployment")
+    res.send("Final Deploy-test commit")
 })
 
 app.get("/info",(req,res)=>{
-    res.send("IT21003332 - Devindu Samarasinghe\nIT21004636 - Nashali Perera\nIT21004322 - Chanukya Serasinghe")
+    res.send("IT21003332 - Devindu Samarasinghe\nIT21004636 - Nashali Perera\nIT21004322 - Chanukya Serasinghes and sons lol")
 })
+
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`)
