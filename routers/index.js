09a7bf866053917c57595678347e72313ac6831b
IT21008382 <it21008382@my.sliit.lk>
2024-04-23 12:42:15 +0530

    routes updated

--- routers/index.js
+++ routers/index.js
@@ -1,7 +1,9 @@
+import adminRouter from './router/Admin.js';
 import itemRouter from './router/Item.js';
 
 
 function routers(app) {
+    app.use('/api/admin', adminRouter);
     app.use('/api/item', itemRouter);
 }
 
