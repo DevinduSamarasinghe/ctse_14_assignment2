9067d0ef0797b9cbb24fd636ead36b6f1a9c2265
DevinduSamarasinghe <devindu990@gmail.com>
2024-04-24 14:04:24 +0530

    cicd-test

--- routers/index.js
+++ routers/index.js
@@ -2,6 +2,9 @@ import itemRouter from './router/Item.js';
 
 function routers(app) {
     app.use('/api/item', itemRouter);
+    app.get('/',(req,res)=>{
+        res.send('Hello World')
+    })
 }
 
 export default routers;
