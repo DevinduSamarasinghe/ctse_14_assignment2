3c3676ce289ac27dbcd68ad5b81fdf3a165f3b58
DevinduSamarasinghe <devindu990@gmail.com>
2024-05-16 21:21:20 +0530

    refactor: new ecs cluster

--- routers/index.js
+++ routers/index.js
@@ -3,7 +3,7 @@ import itemRouter from './router/Item.js';
 function routers(app) {
     app.use('/api/item', itemRouter);
     app.get('/',(req,res)=>{
-        res.send('CTSE-Assignment-2 Group-14')
+        res.send('CTSE-Assignment-2 Group-14 - Testing private and public subnet configuration in aws.')
     })
 }
 
