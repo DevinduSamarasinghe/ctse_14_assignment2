f6915ba70cd0587d2aafa532904fd6f1637f7b47
DevinduSamarasinghe <devindu990@gmail.com>
2024-05-16 20:08:00 +0530

    refactor: ci-cd ecs, service and asg changes

--- routers/index.js
+++ routers/index.js
@@ -3,7 +3,7 @@ import itemRouter from './router/Item.js';
 function routers(app) {
     app.use('/api/item', itemRouter);
     app.get('/',(req,res)=>{
-        res.send('Hello World - final test')
+        res.send('CTSE-Assignment-2 Group-14')
     })
 }
 
