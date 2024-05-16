37bbc49d2d54462893f438d73b94f74d02cdd642
DevinduSamarasinghe <devindu990@gmail.com>
2024-05-17 00:16:44 +0530

    refactor: pushing to zone b

--- routers/index.js
+++ routers/index.js
@@ -3,7 +3,7 @@ import itemRouter from './router/Item.js';
 function routers(app) {
     app.use('/api/item', itemRouter);
     app.get('/',(req,res)=>{
-        res.send('CTSE-Assignment-2 Group-14 - Testing private and public subnet configuration in aws. VPC 2priv 2pub -> Public on, AZ public A NAT on. ')
+        res.send('CTSE-Assignment-2 Group-14 - Testing private and public subnet configuration in aws. VPC 2priv 2pub -> Public on, AZ public A NAT on. One more effort')
     })
 }
 
